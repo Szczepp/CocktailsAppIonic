4fc09c7014692f04a9a98e8d6ad672a30f15ec9b
Szczepan <szczep.kowalik@gmail.com>
2023-04-22 08:13:05 +0200

    Added simple Courses page

--- src/app/Courses/Courses.page.ts
+++ src/app/Courses/Courses.page.ts
@@ -1,21 +1,22 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'Courses',
   templateUrl: 'Courses.page.html',
   styleUrls: ['Courses.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent]
+  imports: [IonicModule, ExploreContainerComponent, NgForOf]
 })
 export class Courses {
   searchTerm: string = '';
   selectedItems: any[] = [];
-  courses: string[] = ['Course 1', 'Course 2', 'Course 3', 'Course 4', 'Course 5'];
+  public courses: string[] = ['Course 1', 'Course 2', 'Course 3', 'Course 4', 'Course 5'];
 
   get filteredCourses() {
     return this.courses.filter(course => course.toLowerCase().includes(this.searchTerm.toLowerCase()));
-  }
+  } 
 }
 
